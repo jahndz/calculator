a6aa85722223dab94e97c55d5a6df0d943aaf378
jahndz <jaguilarhndz@gmail.com>
2015-07-01 16:39:12 -0700

    Modified file

--- calculator.rb
+++ calculator.rb
@@ -6,34 +6,34 @@ loop do
   => Type '3' to Multiply
   => Type '4' to Divide
 "
-answer = gets.chomp
-puts "\n=> What's the first number?"
-num1 = gets.chomp
-puts "\n=> What's the second number?"
-num2 = gets.chomp
+  answer = gets.chomp
+  puts "\n=> What's the first number?"
+  num1 = gets.chomp
+  puts "\n=> What's the second number?"
+  num2 = gets.chomp
 
-case answer
-when '1'
+  case answer
+  when '1'
   operation = num1.to_i + num2.to_i
-  break
-when '2'
+    break
+  when '2'
   operation = num1.to_i - num2.to_i
-  break
-when '3'
+    break
+  when '3'
   operation = num1.to_i * num2.to_i
-  break
-when '4'
+    break
+  when '4'
   operation = num1.to_f / num2.to_f
-  break
-else 
+    break
+  else 
   puts "\nSorry I can't perform that action, please input one of the four."
   end
 end
 
-puts "\n=> The result is: #{operation}"
+  puts "\n=> The result is: #{operation}"
 
-puts "\n=> Would you like to perform another calculation?"
-begin
+  puts "\n=> Would you like to perform another calculation?"
+  begin
     response = gets.chomp
   end until ['y', 'yes', 'n', 'no'].include?(response)
 
